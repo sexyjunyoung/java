c85e5da3c32b979eef5dfdac8196b3c710b458bb
sexyjunyoung <132313856+sexyjunyoung@users.noreply.github.com>
2023-06-04 19:33:25 +0900

    Update jjy.java

--- jjy.java
+++ jjy.java
@@ -1,226 +1,179 @@
 package ch09;
-
 import java.awt.*;
 import javax.swing.*;
 
 public class Calculator extends JFrame {
 
-    private JTextField display;
-    private JLabel history;
-    private JPanel panel;
-    private double operand1, operand2, answer;
-    private String operator;
-
-    public Calculator() {
+    JTextField display;
+    JLabel history;
+    JPanel panel;
+    JButton button;
+    double operand1, operand2, answer;
+    String operator;  
+    Calculator(){
         this.setBounds(100, 100, 300, 500);
         this.setTitle("계산기");
         this.setLayout(new BorderLayout());
-
         display = new JTextField(30);
         display.setText("0");
         display.setFont(new Font("궁서체", Font.BOLD, 35));
-        display.setHorizontalAlignment(SwingConstants.RIGHT);
+        display.setHorizontalAlignment( SwingConstants.RIGHT );
+        
         this.add(display, BorderLayout.NORTH);
-
         history = new JLabel("--");
         this.add(history, BorderLayout.SOUTH);
-
         panel = new JPanel();
-        panel.setLayout(new GridLayout(6, 4));
+        panel.setLayout(new GridLayout(6,4));
 
-        JButton button = new JButton("%");
+        button = new JButton("%");
         panel.add(button);
-
         button = new JButton("CE");
         panel.add(button);
-
         button = new JButton("C");
-        button.addActionListener(e -> {
-            display.setText("0");
+        button.addActionListener(e->{
+            display.setText( "0" );
         });
         panel.add(button);
-
         button = new JButton("<-");
-        button.addActionListener(e -> {
+        button.addActionListener(e->{
             String text = display.getText();
-            if (text.length() > 0) {
+            if (text.length() > 1) {
                 display.setText(text.substring(0, text.length() - 1));
+            } else {
+                display.setText("0");
             }
         });
         panel.add(button);
-
         button = new JButton("1/x");
-        button.addActionListener(e -> {
+        button.addActionListener(e->{
             double d = Double.parseDouble(display.getText());
-            display.setText(1.0 / d + "");
+            display.setText( 1.0 / d + "" );
         });
         panel.add(button);
-
         button = new JButton("x^2");
-        button.addActionListener(e -> {
+        button.addActionListener(e->{
             double d = Double.parseDouble(display.getText());
-            display.setText(d * d + "");
+            display.setText( d * d + "" );
         });
         panel.add(button);
-
         button = new JButton("Sqrt");
-        button.addActionListener(e -> {
+        button.addActionListener(e->{
             double d = Double.parseDouble(display.getText());
-            display.setText(Math.sqrt(d) + "");
+            display.setText( Math.sqrt(d) + "" );
         });
         panel.add(button);
-
         button = new JButton("/");
-        button.addActionListener(e -> {
-            operand1 = Double.parseDouble(display.getText());
-            operator = "/";
-            display.setText("0");
-        });
         panel.add(button);
-
         button = new JButton("7");
-        button.addActionListener(e -> {
-            appendToDisplay("7");
+        button.addActionListener(e->{
+            if( display.getText().equals("0") ) display.setText( "7");
+            else display.setText( display.getText() + "7");
         });
         panel.add(button);
-
         button = new JButton("8");
-        button.addActionListener(e -> {
-            appendToDisplay("8");
+        button.addActionListener(e->{
+            display.setText( display.getText() + "8");
         });
         panel.add(button);
-
         button = new JButton("9");
-        button.addActionListener(e -> {
-            appendToDisplay("9");
+        button.addActionListener(e->{
+            display.setText( display.getText() + "9");
         });
         panel.add(button);
-
         button = new JButton("*");
-        button.addActionListener(e -> {
-            operand1 = Double.parseDouble(display.getText());
-            operator = "*";
-            display.setText("0");
-        });
         panel.add(button);
-
         button = new JButton("4");
-        button.addActionListener(e -> {
-            appendToDisplay("4");
+        button.addActionListener(e->{
+            display.setText( display.getText() + "4");
         });
         panel.add(button);
-
         button = new JButton("5");
-        button.addActionListener(e -> {
-            appendToDisplay("5");
+        button.addActionListener(e->{
+            display.setText( display.getText() + "5");
         });
         panel.add(button);
-
         button = new JButton("6");
-        button.addActionListener(e -> {
-            appendToDisplay("6");
+        button.addActionListener(e->{
+            display.setText( display.getText() + "6");
         });
         panel.add(button);
-
         button = new JButton("-");
-        button.addActionListener(e -> {
-            operand1 = Double.parseDouble(display.getText());
-            operator = "-";
-            display.setText("0");
-        });
         panel.add(button);
-
         button = new JButton("1");
-        button.addActionListener(e -> {
-            appendToDisplay("1");
+        button.addActionListener(e->{
+            display.setText( display.getText() + "1");
         });
         panel.add(button);
-
         button = new JButton("2");
-        button.addActionListener(e -> {
-            appendToDisplay("2");
+        button.addActionListener(e->{
+            display.setText( display.getText() + "2");
         });
         panel.add(button);
-
         button = new JButton("3");
-        button.addActionListener(e -> {
-            appendToDisplay("3");
+        button.addActionListener(e->{
+            display.setText( display.getText() + "3");
         });
         panel.add(button);
-
         button = new JButton("+");
-        button.addActionListener(e -> {
-            operand1 = Double.parseDouble(display.getText());
-            operator = "+";
+        button.addActionListener(e->{
+            operand1 = Double.parseDouble( display.getText() );
             display.setText("0");
+            operator = "+";
         });
         panel.add(button);
-
         button = new JButton("+/-");
-        button.addActionListener(e -> {
-            String text = display.getText();
-            if (!text.equals("0")) {
-                if (text.startsWith("-")) {
-                    display.setText(text.substring(1));
+        button.addActionListener(e->{
+            if( !display.getText().equals("0") ) {
+                if( display.getText().contains("-") ) {
+                    display.setText( display.getText().substring(1) );                
                 } else {
-                    display.setText("-" + text);
+                    display.setText( "-" + display.getText() );                
                 }
             }
         });
         panel.add(button);
-
         button = new JButton("0");
         button.addActionListener(e -> {
-            appendToDisplay("0");
+            if( !display.getText().equals("0") ) { 
+                display.setText( display.getText() + "0");
+            }
         });
         panel.add(button);
-
         button = new JButton(".");
-        button.addActionListener(e -> {
-            String text = display.getText();
-            if (!text.contains(".")) {
-                display.setText(text + ".");
+        button.addActionListener(e->{
+            if( !display.getText().contains(".") ) {
+                display.setText( display.getText() + ".");
             }
         });
         panel.add(button);
-
         button = new JButton("=");
-        button.addActionListener(e -> {
-            operand2 = Double.parseDouble(display.getText());
-            switch (operator) {
-                case "+":
-                    answer = operand1 + operand2;
-                    break;
-                case "-":
-                    answer = operand1 - operand2;
-                    break;
-                case "*":
-                    answer = operand1 * operand2;
-                    break;
-                case "/":
-                    answer = operand1 / operand2;
-                    break;
-                default:
-                    break;
+        button.addActionListener(e-> {
+            operand2 = Double.parseDouble( display.getText() );
+            switch( operator ) {
+            case "+":
+                answer = operand1 + operand2;
+                break;
+            case "-":
+                answer = operand1 - operand2;
+                break;
+            case "*":
+                answer = operand1 * operand2;
+                break;
+            case "/":
+                answer = operand1 / operand2;
+                break;
+            default:
+                break;
             }
-            display.setText(answer + "");
+            display.setText( answer + "");
         });
         panel.add(button);
 
-        this.add(panel, BorderLayout.CENTER);
+        this.add(panel, BorderLayout.CENTER); 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
     }
-
-    private void appendToDisplay(String text) {
-        String currentText = display.getText();
-        if (currentText.equals("0")) {
-            display.setText(text);
-        } else {
-            display.setText(currentText + text);
-        }
-    }
-
+    
     public static void main(String[] args) {
         new Calculator();
     }
